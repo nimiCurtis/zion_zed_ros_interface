999d4733a7171f0c30d76c52b0c8f805718ed914
nimiCurtis <nimicu21@gmail.com>
2024-06-15 16:38:55 +0300

    update

--- scripts/recording/zed_recording_node.py
+++ scripts/recording/zed_recording_node.py
@@ -92,7 +92,7 @@ class ZedRecordManager(object):
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdownhook)
 
-        self._target_obj_topic = topics.get("target_object",None)
+        self._target_obj_topic = topics.get("target_object") if "target_object" in topics_list_keys else None
         self._is_recording = False
         self._ros_start_time = rospy.Time.now()
         self._ros_current_time = rospy.Time.now()
